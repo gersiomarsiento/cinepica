c36e2a9325d2100327bd75db301c8862270991fa
gersiomarsiento <gersiomarsiento@gmail.com>
2022-09-04 20:42:40 -0300

    Cleaned CSS; samesite:Lax; cookie:secure

--- public/scripts/nav-imdb-scripts.js
+++ public/scripts/nav-imdb-scripts.js
@@ -1,10 +1,11 @@
-//**** IMDB Rating Plugin Script ****
+// IMDB Rating Plugin Script 
 try{
   (function(d,s,id){var js,stags=d.getElementsByTagName(s)[0];if(d.getElementById(id)){return;}js=d.createElement(s);js.id=id;js.src="https://ia.media-imdb.com/images/G/01/imdb/plugins/rating/js/rating.js";stags.parentNode.insertBefore(js,stags);})(document,"script","imdb-rating-api");
 } catch {
   (err)=>{console.log(err)}
 }
 
+// Hamburger
 const showNav = () => {
   document.getElementById("navbar").classList.toggle("display-navbar");
   document.body.classList.toggle("modal-open");
@@ -15,10 +16,11 @@ const showNav = () => {
 };
 document.querySelector("#hamburger-container").addEventListener('click', showNav);
 
-
-
+// User button
 let collapsibles = document.querySelectorAll('.collapsible');
+
 const toggleUserMenu = ()=>{
-  collapsibles.forEach(x => x.classList.toggle("collapsible"))
+  collapsibles.forEach(x => x.classList.toggle("collapsible"));
+  return false
 }
 document.querySelector("#user-btn-container").addEventListener('click', toggleUserMenu);
